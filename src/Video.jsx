83a60bbf24524ff828d4e026fcbafe850a0bd0e9
lilxak <zakben45@gmail.com>
2021-08-29 10:58:56 +0100

    Create Nator animation

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,4 +1,5 @@
 import { Composition } from 'remotion';
+import { Nator } from './components/title/Nator';
 import { Term } from './components/title/Term';
 
 export const RemotionVideo = () => {
@@ -7,6 +8,14 @@ export const RemotionVideo = () => {
 			<Composition
 				id="Term"
 				component={Term}
+				width={1920}
+				height={1080}
+				durationInFrames={2910}
+				fps={30}
+			/>
+			<Composition
+				id="Nator"
+				component={Nator}
 				width={1920}
 				height={1080}
 				durationInFrames={2910}
--- src/components/title/Nator.jsx
+++ src/components/title/Nator.jsx
@@ -1,8 +1,8 @@
 import { interpolate, useCurrentFrame } from "remotion"
 
-export const Term = () => {
+export const Nator = () => {
 	const frame = useCurrentFrame();
-	const marginRight = interpolate(frame, [0,2670], [0, 6350], {
+	const marginRight = interpolate(frame, [0,2670], [0, 6950], {
 		extrapolateRight: 'clamp',
 	  })
 
@@ -10,14 +10,14 @@ export const Term = () => {
 		<h1 style={{
 			position: "absolute",
 			top: '-100px',
-			left: `${1920-marginRight}px`,
+			left: `${0-6072+marginRight}px`,
 			fontSize: "1080px",
 			margin: 0,
 			padding: 0,
 			fontFamily: "terminator real nfi"
 		}}
 		>
-			TERMI
+			Nator
 		</h1>
 	)
 }
